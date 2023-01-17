be0cf2774fa9056cf554b2ee716263f04c5aa8f7
meshkova-if <121893822+meshkova-if@users.noreply.github.com>
2023-01-17 10:48:21 +0500

    доработка

--- game_v3.py
+++ game_v3.py
@@ -45,9 +45,6 @@ def score_game(predict) -> int:
     print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
     return(score)
 
-
+#RUN
 if __name__ == '__main__':
-# RUN
-score_game(random_predict)
-
-
+    score_game(random_predict)
